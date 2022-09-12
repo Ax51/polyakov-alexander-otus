ffed4ffbc89336b9f3974d798dcf391422dd23a0
Ax51 <80718359+Ax51@users.noreply.github.com>
2022-09-12 14:57:00 +0300

    Improved getDayName

--- hometasks/react-routing/src/utils/getDayName.ts
+++ hometasks/react-routing/src/utils/getDayName.ts
@@ -1,20 +1,4 @@
 export function getDayName(day = new Date().getDay()) {
-  switch (day) {
-    case 0:
-      return 'Sunday'
-    case 1:
-      return 'Monday'
-    case 2:
-      return 'Tuesday'
-    case 3:
-      return 'Wednesday'
-    case 4:
-      return 'Thursday'
-    case 5:
-      return 'Friday'
-    case 6:
-      return 'Saturday'
-    default:
-      return 'Day'
-  }
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  return dayNames[day] ?? 'Day'
 }
